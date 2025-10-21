73ba6b8b3a578774a02a70f8ab8ba9220ef4c65e
Allan Maia <allan.santos@bsatech.io>
2025-10-20 21:26:24 -0300

    unidade 3

--- tests/Alura.Adopet.Testes/Comandos/FabricaDeComandosTest.cs
+++ tests/Alura.Adopet.Testes/Comandos/FabricaDeComandosTest.cs
@@ -16,6 +16,17 @@ public class FabricaDeComandosTest
     }
 
     [Fact]
+    public void DadoUmParametroDeveRetornarUmTipoList()
+    {
+        //Arrange
+        string[] args = { "list", "lista.csv" };
+        //Act
+        var comando = ComandosFactory.CriarComando(args);
+        //Assert
+        Assert.IsType<List>(comando);
+    }
+
+    [Fact]
     public void DadoUmParametroInvalidoDeveRetornarNulo()
     {
         //Arrange
@@ -45,6 +56,23 @@ public class FabricaDeComandosTest
         var comando = ComandosFactory.CriarComando(args);
         //Assert
         Assert.Null(comando);
+    }
+
+    [Theory]
+    [InlineData("import", "Import")]
+    [InlineData("import-clientes", "ImportClientes")]
+    [InlineData("show", "Show")]
+    [InlineData("list", "List")]
+    [InlineData("help", "Help")]
+    public void DadoParametroValidoDeveRetornarObjetoNaoNulo(string instrucao, string nomeTipo)
+    {
+        // arrange
+        string[] args = new[] { instrucao, "lista.csv" };
+        // act
+        var comando = ComandosFactory.CriarComando(args);
+        // assert
+        Assert.NotNull(comando);
+        Assert.Equal(nomeTipo, comando.GetType().Name);
     }
 
 
